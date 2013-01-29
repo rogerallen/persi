728c5656958036559f287e07657229a976db8fb5
Roger Allen <rallen@gmail.com>
2013-01-28 18:51:03 -0800

    Basic project structure.

--- README.md
+++ README.md
@@ -1,13 +1,16 @@
 # persi
 
-A Clojure library designed to ... well, that part is up to you.
+A Clojure library designed to save data to files in a directory.  I
+want to use this to keep a simple record of midi keyboard events.  I
+might be reinventing the wheel, but I also consider this a learning
+excercise.
 
 ## Usage
 
-FIXME
+TBD
 
 ## License
 
-Copyright © 2013 FIXME
+Copyright © 2013 Roger Allen (rallen@gmail.com)
 
 Distributed under the Eclipse Public License, the same as Clojure.
--- project.clj
+++ project.clj
@@ -1,6 +1,9 @@
 (defproject persi "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A persistent directory for clojure program events."
+  :url "http://github.com/rogerallen/persi"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :plugins [ [lein-swank "1.4.5"] ]
+  :jvm-opts ["-Dfile.encoding=UTF-8"]
+)
