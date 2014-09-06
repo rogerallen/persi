a83f1feee23627d9e0636c80b004cf5cbed54da3
Roger Allen <rallen@gmail.com>
2014-09-06 08:46:34 -0700

    moving to 0.2.0 just to get this to clojars

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject persi "0.1.0-SNAPSHOT"
-  :description "A persistent directory for clojure program events."
+(defproject persi "0.2.0-SNAPSHOT"
+  :description "A simple, limited persistence library for clojure."
   :url "http://github.com/rogerallen/persi"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :dependencies [[org.clojure/clojure "1.6.1"]]
   :jvm-opts ["-Dfile.encoding=UTF-8"]
 )
